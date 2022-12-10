4215e716badfaa414ec9f449d74deb61bb649fe9
hami-r <mynameissaitama54@gmail.com>
2022-12-10 14:47:10 +0530

    complaint api

---
+++ src/main/java/com/nest/complaintapp_backend/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.nest.complaintapp_backend.controller;
+
+import com.nest.complaintapp_backend.model.UserModel;
+import com.nest.complaintapp_backend.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDao userdao;
+
+    @GetMapping("/")
+    public String home(){
+        return "Welcome to the complaint api";
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/useradd",consumes = "application/json",produces = "application/json")
+    public HashMap<String, String> userReg(@RequestBody UserModel user){
+        userdao.save(user);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status","success");
+        return map;
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userverify",consumes = "application/json",produces = "application/json")
+    public HashMap<String, String> userVerify(@RequestBody UserModel user){
+        HashMap<String, String> map = new HashMap<>();
+        List<UserModel> result = (List<UserModel>) userdao.userVerify(user.getUsername(),user.getPassword());
+        if(result.size()>0){
+            map.put("status","success");
+            map.put("userId",String.valueOf(result.get(0).getId()));
+        }
+        return map;
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/viewprofile",consumes = "application/json",produces = "application/json")
+    public List<UserModel> viewProfile(@RequestBody UserModel user){
+        System.out.println(user.getId());
+         return (List<UserModel>) userdao.viewProfile(String.valueOf(user.getId()));
+    }
+}
